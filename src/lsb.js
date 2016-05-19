9fe0ad7bdbf88bd2f8d6550e08ff05fea01461d6
Alexander Emashev <a.yemashev@obespechenie-vniief.ru>
2016-05-19 17:13:10 +0300

    Add touch support.

--- src/lsb.js
+++ src/lsb.js
@@ -13,7 +13,7 @@
       showDownloadButton: true
     };
     var settings = $.extend(defaultSettings, options);
-    
+
     // Lightbox element..
     var $lsb;
     // Wait cursor.
@@ -24,6 +24,8 @@
     var $next;
     // Previous image button.
     var $prev;
+    // Close button
+    var $close;
     // Image download button.
     var $download;
     // Used for transition effect between slides.
@@ -53,7 +55,7 @@
         } else {
           images = $('.lightspeed-preview:not([data-lsb-group])');
         }
-        
+
         var hrefCollection = [];
         var currentImage = null;
 
@@ -67,7 +69,7 @@
 
         this.collection = hrefCollection;
         this.current = currentImage;
-        
+
         // Hide next and previous buttons if there is only one image.
         if (this.collection.length === 1) {
           $prev.css('visibility', 'hidden');
@@ -125,7 +127,7 @@
 
       var i;
       for (i = 0; i < 12; i++) {
-        spinCircle += '<div class="spin-circle"></div>';
+        spinCircle += '<div class="waitingicon-circle"></div>';
       }
 
       $('body').append(
@@ -134,39 +136,49 @@
         '<div class="lsb-image-container">' +
         '<img class="lsb-image lsb-noimage">' +
         '</div>' +
-        '<div class="fading-spinner wait-icon">' +
+        '<div class="waitingicon">' +
         spinCircle +
         '</div>' +
-        '<div class="lsb-controls">' +
-        '<span class="lsb-control lsb-prev" title="Previous image"></span>' +
-        '<a class="lsb-control lsb-download" href="#" target="_blank" download title="Download image"></a>'+
-        '<span class="lsb-control lsb-next" title="Next image"></span>' +
-        '</div>' +
+        '<div class="lsb-control lsb-close"><span class="lsb-control-text">˟</span></div>' +
+        '<div class="lsb-control lsb-prev"><span class="lsb-control-text">&lt;</span></div>' +
+        '<div class="lsb-control lsb-next"><span class="lsb-control-text">&gt;</span></div>' +
         '</div>' +
         '</div>'
       );
 
       // Lightbox element.
       $lsb = $('.lightspeed-box');
-      $spinner = $('.fading-spinner');
+      $spinner = $('.waitingicon');
       $lsbImage = $lsb.find('.lsb-image');
       // Next image button.
       $next = $lsb.find('.lsb-next');
       // Previous image button.
       $prev = $lsb.find('.lsb-prev');
+      $close = $lsb.find('.lsb-close');
       $download = $lsb.find('.lsb-download');
-      
+
       if (!settings.showDownloadButton) {
         $download.css('display', 'none');
       }
 
+      // Add swipe detection plugin
+      $lsb.swipeDetector().on('swipeLeft.lsb swipeRight.lsb', function (event) {
+        if (imageCollection.collection.length > 1) {
+          if (event.type === 'swipeLeft') {
+            switchImage(imageCollection.nextImage());
+          } else if (event.type === 'swipeRight') {
+            switchImage(imageCollection.previousImage());
+          }
+        }
+      });
+
       ///// Add event handlers for elements.
-      
+
       /**
        * Next image button click.
        */
       $next.click(function (event) {
-        event.preventDefault();
+        event.stopPropagation();
         var img = imageCollection.nextImage();
         switchImage(img);
       });
@@ -175,24 +187,29 @@
        * Previous image button click.
        */
       $prev.click(function (event) {
-        event.preventDefault();
+        event.stopPropagation();
         var img = imageCollection.previousImage();
         switchImage(img);
+      });
+
+      $lsbImage.click(function (event) {
+        event.stopPropagation();
+        if (imageCollection.collection.length > 1) {
+          switchImage(imageCollection.nextImage());
+        } else {
+          closeLightbox();
+        }
       });
 
       /**
        * Click on empty space of lightbox.
        */
       $lsb.click(function (event) {
-        // Return if lightbox controls were clicked.
-        if ($(event.target.parentElement).hasClass('lsb-controls')) {
-          return;
-        }
-        
+        console.log('Close clicked', event);
         closeLightbox();
       });
     })();
-    
+
     /**
      * Shows lightbox.
      */
@@ -200,23 +217,23 @@
       $lsb.addClass('lsb-active');
     }
     /**
-    * Hides lightbox.
-    */
+     * Hides lightbox.
+     */
     function closeLightbox() {
       $lsb.removeClass('lsb-active');
       $lsbImage.removeClass('lsb-image-loaded');
       $lsbImage.addClass('lsb-noimage');
     }
-    
+
     /**
-    * Shows image when animation has finished.
-    */
+     * Shows image when animation has finished.
+     */
     function displayImage() {
-      $spinner.css('opacity', 0);
+      $spinner.css('display', 'none');
       $lsbImage.removeClass('lsb-noimage');
       $lsbImage.addClass('lsb-image-loaded');
     }
-    
+
     /**
      * Switches image to specific.
      * @param href image reference.
@@ -224,16 +241,16 @@
     function switchImage(href) {
       $lsbImage.addClass('lsb-noimage');
       $lsbImage.removeClass('lsb-image-loaded');
-      
+
       // Use timeout to let the image transition effect play.
       window.setTimeout(function () {
         loadImage(href);
       }, transitionTimeout);
     }
-    
-    
+
+
     function loadImage(href) {
-      $spinner.css('opacity', 1);
+      $spinner.css('display', 'block');
       //Load image.
       var $img = $('<img />').attr('src', href).on('load', function () {
         if (!this.complete || typeof this.naturalWidth === "undefined" || this.naturalWidth === 0) {
@@ -260,9 +277,91 @@
       var group = $(this).attr('data-lsb-group');
       // Get all images to set.
       imageCollection.getImagesInSet(fullSizeHref, group);
-      
+
       showLightbox();
       switchImage(fullSizeHref);
     });
   };
+
+  /** Plugin to detect swipes
+   */
+  $.fn.swipeDetector = function (options) {
+    // States: 0 - no swipe, 1 - swipe started, 2 - swipe released
+    var swipeState = 0;
+    // Coordinates when swipe started
+    var startX = 0;
+    var startY = 0;
+    // Distance of swipe
+    var pixelOffsetX = 0;
+    var pixelOffsetY = 0;
+    // Target element which should detect swipes.
+    var swipeTarget = this;
+    var defaultSettings = {
+      // Amount of pixels, when swipe don't count.
+      swipeTreshold: 20
+    };
+
+    // Initializer
+    (function init() {
+      options = $.extend(defaultSettings, options);
+      // Support touch and mouse as well.
+      swipeTarget.on('mousedown touchstart', swipeStart);
+      $('html').on('mouseup touchend', swipeEnd);
+      $('html').on('mousemove touchmove', swiping);
+    })();
+
+    function swipeStart(event) {
+      if (event.originalEvent.touches)
+        event = event.originalEvent.touches[0];
+
+      if (swipeState === 0) {
+        swipeState = 1;
+        startX = event.clientX;
+        startY = event.clientY;
+      }
+    }
+
+    function swipeEnd(event) {
+      if (swipeState === 2) {
+        swipeState = 0;
+
+        if (Math.abs(pixelOffsetX) > Math.abs(pixelOffsetY)) { // Horizontal Swipe
+          if (pixelOffsetX < 0) {
+            swipeTarget.trigger($.Event('swipeLeft.lsb'));
+          } else {
+            swipeTarget.trigger($.Event('swipeRight.lsb'));
+          }
+        } else { // Vertical swipe
+          if (pixelOffsetY < 0) {
+            swipeTarget.trigger($.Event('swipeUp.lsb'));
+          } else {
+            swipeTarget.trigger($.Event('swipeDown.lsb'));
+          }
+        }
+      }
+    }
+
+    function swiping(event) {
+      // If swipe don't occuring, do nothing.
+      if (swipeState !== 1)
+        return;
+
+
+      if (event.originalEvent.touches) {
+        event = event.originalEvent.touches[0];
+      }
+
+      var swipeOffsetX = event.clientX - startX;
+      var swipeOffsetY = event.clientY - startY;
+
+      if ((Math.abs(swipeOffsetX) > options.swipeTreshold) ||
+        (Math.abs(swipeOffsetY) > options.swipeTreshold)) {
+        swipeState = 2;
+        pixelOffsetX = swipeOffsetX;
+        pixelOffsetY = swipeOffsetY;
+      }
+    }
+
+    return swipeTarget; // Return element available for chaining.
+  }
 }(jQuery));
